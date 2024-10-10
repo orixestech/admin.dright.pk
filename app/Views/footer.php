47874ac5e3aabb7fddbb336435184ab95c8113f1
orixestech <info@orixestech.com>
2024-10-11 00:25:08 +0500

    ...

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,23 +1,12 @@
 </div>
 
 <footer class="content-footer">
-    <div>© 2020 Baston - <a href="http://laborasyon.com" target="_blank">Laborasyon</a></div>
-    <div>
-        <nav class="nav">
-            <a href="https://themeforest.net/licenses/standard" class="nav-link">Licenses</a>
-            <a href="#" class="nav-link">Change Log</a>
-            <a href="#" class="nav-link">Get Help</a>
-        </nav>
-    </div>
+    <div>© 2024 D-Right - <a href="https://dright.net/" target="_blank">D-Right Technologies</a></div>
 </footer>
 </div>
 </div>
 </div>
-
-
 <script src="<?= $template ?>vendors/datepicker/daterangepicker.js"></script>
-
-
 <script src="<?= $template ?>assets/js/app.min.js"></script>
 </body>
 </html>
